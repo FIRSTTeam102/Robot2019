f901508bae859c4afba3d3314140055dfa20abb9
GEARHEADS-6\first <firstteam102@gmail.com>
2019-01-15 20:13:24 -0500

    Some documentation

---
+++ src/main/java/org/team102/robots/robot2019/commands/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * The code for FRC Team #102's robot for the 2019 game, Destination: Deep Space
+ * Copyright (C) 2019  Robotics Fund Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * Contact us at: firstteam102@gmail.com
+ */
+
+/**
+ * This package is for any of the robot's commands.<br>
+ * All commands extend {@link edu.wpi.first.wpilibj.command.Command the command class} or one of the derivatives thereof.<br>
+ * <br>
+ * A command implements a specific function for a specific {@link org.team102.robots.robot2019.subsystems portion} of the robot;<br>
+ * it could be as simple as opening a claw or as complex as navigating to a specific location, or anything in between.
+ */
+package org.team102.robots.robot2019.commands;
