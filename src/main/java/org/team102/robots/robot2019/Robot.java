4bca9d7f03aa4d28525677728ecb0876c81c2bc9
gearheads4 <firstteam102@gmail.com>
2019-01-19 09:33:34 -0500

    Un-track the sim config files because they are dynamically generated; Spencer's test code (commented)

--- src/main/java/org/team102/robots/robot2019/Robot.java
+++ src/main/java/org/team102/robots/robot2019/Robot.java
@@ -20,9 +20,15 @@
 
 package org.team102.robots.robot2019;
 
+
 import org.team102.robots.robot2019.subsystems.*;
 
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
 
 public class Robot extends TimedRobot {
 	public static SubsystemDriveTrain driveTrain;
@@ -33,12 +39,47 @@ public class Robot extends TimedRobot {
 	
 	public static OI oi;
 	
-	// ALWAYS include this!!
+	
+	int testIntA;
+	int testIntB;
+	int testIntC;
+	boolean testBoolA;
+	
+	// AWLAYS include this!!
 	public static void main(String[] args) {
 		TimedRobot.startRobot(Robot::new);
 	}
 	
 	public void robotInit() {
 		oi = new OI();
+		
+		/*testIntA = 0;
+		testIntB = 0;
+		testIntC = 10;
+		testBoolA = false;
+
+		SmartDashboard.putNumber("Test Int A", testIntA);
+		SmartDashboard.putNumber("Test Int B", testIntB);
+		SmartDashboard.putNumber("Test Int C", testIntC);
+		SmartDashboard.putBoolean("Test Bool A", testBoolA);*/
 	}
+	
+	@Override
+	public void robotPeriodic() {
+		
+		/*testBoolA = SmartDashboard.getBoolean("Test Bool A", false);
+		testIntC = (int)SmartDashboard.getNumber("Test Int C", 10);
+		
+		if(testBoolA) {
+			testIntA = (int)(Math.random()*testIntC);
+		}
+		
+		testIntB = testIntA;
+		
+		SmartDashboard.putNumber("Test Int A", testIntA);
+		SmartDashboard.putNumber("Test Int B", testIntB);*/
+		
+	}
+	
+	
 }
