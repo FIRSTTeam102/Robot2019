edb9b7aacbbb1bb35ac4c62578e29aea053dd347
GEARHEADS-6\first <firstteam102@gmail.com>
2019-02-04 19:51:43 -0500

    Documentation and license information (trivial)

--- src/main/java/org/team102/robots/robot2019/lib/ArduinoConnection.java
+++ src/main/java/org/team102/robots/robot2019/lib/ArduinoConnection.java
@@ -1,3 +1,23 @@
+/*
+ * The code for FRC Team #102's robot for the 2019 game, Destination: Deep Space
+ * Copyright (C) 2019  Robotics Fund Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * Contact us at: firstteam102@gmail.com
+ */
+
 package org.team102.robots.robot2019.lib;
 
 import java.io.Closeable;
@@ -6,8 +26,11 @@ import java.util.function.Consumer;
 
 import com.fazecast.jSerialComm.SerialPort;
 
-public class ArduinoConnection implements Closeable {
-	public static final String EOL = "\n";
+/**
+ * A connection to an Arduino via a serial port
+ */
+public class ArduinoConnection implements Closeable, AutoCloseable {
+	private String eol = "\n";
 	
 	private SerialPort port;
 	private String dataBuff = "";
@@ -15,6 +38,10 @@ public class ArduinoConnection implements Closeable {
 	private ArrayList<String> lines = new ArrayList<>();
 	private Consumer<String> lineListener = null;
 	
+	/**
+	 * Create the connection
+	 * @param portID The name of the serial port, i.e. {@code /dev/tty0}
+	 */
 	public ArduinoConnection(String portID) {
 		try {
 			port = SerialPort.getCommPort(portID);
@@ -38,15 +65,65 @@ public class ArduinoConnection implements Closeable {
 		port.closePort();
 	}
 	
+	/**
+	 * Gives back the currently-used end-of-line marker, which is used for parsing and writing lines
+	 * @return The current end-of-line marker
+	 */
+	public String getEOL() {
+		return eol;
+	}
+	
+	/**
+	 * Sets the end-of-line marker used for parsing and writing lines
+	 * @param eol The new end-of-line marker
+	 */
+	public void setEOL(String eol) {
+		this.eol = eol;
+	}
+	
+	/**
+	 * Gives back whether or not there are more lines to be manually queried from the Arduino.
+	 * <strong>You can only query the lines manually if the {@link #setLineListener(Consumer) line listener} is null.</strong>
+	 * @return Whether or not there are any lines available to be manually queried
+	 */
+	public boolean hasLines() {
+		return !lines.isEmpty();
+	}
+	
+	/**
+	 * Gives back the oldest available line (FIFO) from the Arduino.
+	 * <strong>You can only query the lines manually if the {@link #setLineListener(Consumer) line listener} is null.</strong>
+	 * @return The oldest available line, or {@code null} if none are available
+	 */
+	public String getLine() {
+		if(!hasLines()) {
+			return null;
+		} else {
+			return lines.remove(0);
+		}
+	}
+	
+	/**
+	 * Sets the listener for new lines of data from the Arduino.
+	 * <strong>You can only query the lines manually if this is null.</strong>
+	 * @param lineListener The listener
+	 */
 	public void setLineListener(Consumer<String> lineListener) {
 		this.lineListener = lineListener;
 	}
 	
+	/**
+	 * Writes the given line of text to the Arduino, then a new-line character
+	 * @param line The line
+	 */
 	public void write(String line) {
-		byte[] buff = (line + EOL).getBytes();
+		byte[] buff = (line + eol).getBytes();
 		port.writeBytes(buff, buff.length);
 	}
 	
+	/**
+	 * Polls the serial port for new data from the Arduino
+	 */
 	public void update() {
 		int bytesAvailable = port.bytesAvailable();
 		
@@ -57,9 +134,9 @@ public class ArduinoConnection implements Closeable {
 			dataBuff += new String(buff);
 		}
 		
-		if(dataBuff.contains(EOL)) {
-			boolean useLastToken = dataBuff.endsWith(EOL);
-			String[] tokens = dataBuff.split(EOL);
+		if(dataBuff.contains(eol)) {
+			boolean useLastToken = dataBuff.endsWith(eol);
+			String[] tokens = dataBuff.split(eol);
 			int max = tokens.length - 1;
 			
 			if(useLastToken) {
