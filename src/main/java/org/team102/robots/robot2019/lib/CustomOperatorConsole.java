42767f46a853deb1a07490b5f05fb12604df32a4
GEARHEADS-6\first <firstteam102@gmail.com>
2019-02-18 16:53:45 -0500

    Fixed the bitwise logic for the operator console light patterns

--- src/main/java/org/team102/robots/robot2019/lib/CustomOperatorConsole.java
+++ src/main/java/org/team102/robots/robot2019/lib/CustomOperatorConsole.java
@@ -50,9 +50,9 @@ public class CustomOperatorConsole {
 	}
 	
 	public void setLightPattern(int patternID) {
-		int patternOutputBits = patternID
-				<< 22 // Shift the data bits to positions 7 to 10 (they should currently be in bits 29 to 32)
-				& 0b00000011110000000000000000000000; // Clear all the bits except 7 to 10
+		int patternOutputBits = ~patternID
+				<< 6 // Shift the data bits to the correct
+				& 0b00000000000000000000001111000000; // Clear all the bits except the ones we want
 		
 		connection.setOutputs(patternOutputBits);
 	}
